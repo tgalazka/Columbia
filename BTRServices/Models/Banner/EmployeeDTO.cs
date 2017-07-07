195880c8d76487d8528fdd379cd3664b75a0b4f1
K Cooper <kj07208@gmail.com>
2017-07-07 14:17:33 -0400

    Fixed the banner employee object to return supervisor and supervisor uni updated connection strings to replace when deploying to a particular target

--- BTRServices/Models/Banner/EmployeeDTO.cs
+++ BTRServices/Models/Banner/EmployeeDTO.cs
@@ -7,11 +7,14 @@ namespace BTRServices.Models.Banner
 {
     public class EmployeeDTO
     {
+        public int employee_key_id { get; set; }
         public string uni { get; set; }
         public string last_name { get; set; }
         public string first_name { get; set; }
         public string position_number { get; set; }
         public string position_description { get; set; }
         public string position_suffix { get; set; }
+        public string supervisor_uni { get; set; }
+        public string supervisor { get; set; }
     }
 }
