a2775bf71f5ce726ab6b76dc73c07199f7ba924f
Thomas J. Galazka <tjgalazka@gmail.com>
2017-07-07 12:58:09 -0400

    Added an refactored, tested, non-Exception based Utils.HTTP.QueryString class to get my feat wet.
    Must add wrapper tests around the original GUID method to complete refactoring.

---
+++ BTRServices/Utils/HTTP/QueryString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTRServices.Utils.HTTP
+{
+    /// <summary>
+    /// Utility class for manipulating a query string from an HTTP request.
+    /// </summary>
+    public class QueryString
+    {
+        private QueryString() { }
+
+        /// <summary>
+        /// Retrieves the value of the first matching key in the supplied query string, or NULL. 
+        /// </summary>
+        /// <param name="qs">Request query string parameters, if any.</param>
+        /// <param name="key">Target key for value retrieval.</param>
+        /// <returns>String value of first matching key, or NULL.</returns>
+        public static string GetValue(IEnumerable<KeyValuePair<string, string>> qs, string key)
+        {
+            return (qs == null || key == null || !qs.Any()) ?
+                null :
+                qs
+                .Where(pair => key.Equals(pair.Key))
+                .Select(pair => pair.Value)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Retrieves the integer (32) value of the first matching key in the supplied query string, or NULL. 
+        /// </summary>
+        /// <param name="qs">Request query string parameters, if any.</param>
+        /// <param name="key">Target key for integer value retrieval.</param>
+        /// <returns>Integer value of first matching key, or NULL.</returns>
+        public static Nullable<int> GetIntValue(IEnumerable<KeyValuePair<string, string>> qs, string key)
+        {
+            string val = GetValue(qs, key);
+            int value;
+            return Int32.TryParse(val, out value) ? value : new Nullable<int>();
+        }
+
+        /// <summary>
+        /// Retrieves the Guid value of the first matching key in the supplied query string, or Guid.Empty. 
+        /// </summary>
+        /// <param name="qs">Request query string parameters, if any.</param>
+        /// <param name="key">Target key for integer value retrieval.</param>
+        /// <returns>Guid value of first matching key, or Guid.Empty.</returns>
+        public static Guid GetGuidValue(IEnumerable<KeyValuePair<string, string>> qs, string key)
+        {
+            string val = GetValue(qs, key);
+            Guid value;
+            Guid.TryParse(val, out value);
+            return value;
+        }
+    }
+}
---
+++ BtrServicesUnitTest/Utils/HTTP/QueryStringTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BTRServices.Utils.HTTP;
+using System.Collections.Generic;
+
+namespace BtrServicesUnitTest.Utils.HTTP
+{
+    [TestClass]
+    public class QueryStringTest
+    {
+        [TestMethod]
+        public void Utils__HTTP__QueryString___GetValue()
+        {
+            Assert.IsNull(QueryString.GetValue(null, null), "NULL query string failed to return NULL.");
+            Assert.IsNull(QueryString.GetValue(new Dictionary<string, string>(), null), "NULL key failed to return NULL.");
+            Assert.IsNull(QueryString.GetValue(new Dictionary<string, string>(), "baz"), "Empty query string failed to return NULL.");
+            Assert.IsNull(QueryString.GetValue(new Dictionary<string, string> { { "foo", "bar" } }, "baz"), "Absent key failed to return NULL.");
+            Assert.IsNull(QueryString.GetValue(new Dictionary<string, string> { { "foo", "bar" } }, "FOO"), "Case-sensitive absent key failed to return NULL.");
+            Assert.AreEqual("bar", QueryString.GetValue(new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("foo", "bar"),
+                new KeyValuePair<string, string>("foo", "baz")
+            }, "foo"), "Present key failed to return value.");
+        }
+
+        [TestMethod]
+        public void Utils__HTTP__QueryString___GetIntValue()
+        {
+            Assert.IsNull(QueryString.GetIntValue(null, null));
+            Assert.IsNull(QueryString.GetIntValue(new Dictionary<string, string>(), null));
+            Assert.IsNull(QueryString.GetIntValue(new Dictionary<string, string>(), "baz"));
+            Assert.IsNull(QueryString.GetIntValue(new Dictionary<string, string> { { "foo", "bar" } }, "baz"));
+            Assert.IsNull(QueryString.GetIntValue(new Dictionary<string, string> { { "foo", "bar" } }, "FOO"));
+            Assert.IsNull(QueryString.GetIntValue(new Dictionary<string, string> { { "foo", "bar" } }, "foo"));
+
+            Assert.AreEqual(0, QueryString.GetIntValue(new Dictionary<string, string> { { "foo", "0" } }, "foo"));
+            Assert.AreEqual(-1, QueryString.GetIntValue(new Dictionary<string, string> { { "foo", "-1" } }, "foo"));
+            Assert.AreEqual(100, QueryString.GetIntValue(new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("foo", "100"),
+                new KeyValuePair<string, string>("foo", "1")
+            }, "foo"), "Failed to retrieve first matching key value.");
+        }
+
+        [TestMethod]
+        public void Utils__HTTP__QueryString___GetGuidValue()
+        {
+            Assert.AreEqual(Guid.Empty, QueryString.GetGuidValue(null, null));
+            Assert.AreEqual(Guid.Empty, QueryString.GetGuidValue(new Dictionary<string, string>(), null));
+            Assert.AreEqual(Guid.Empty, QueryString.GetGuidValue(new Dictionary<string, string>(), "baz"));
+            Assert.AreEqual(Guid.Empty, QueryString.GetGuidValue(new Dictionary<string, string> { { "foo", "bar" } }, "baz"));
+            Assert.AreEqual(Guid.Empty, QueryString.GetGuidValue(new Dictionary<string, string> { { "foo", "bar" } }, "FOO"));
+            Assert.AreEqual(Guid.Empty, QueryString.GetGuidValue(new Dictionary<string, string> { { "foo", "bar" } }, "foo"));
+
+            Guid expected = Guid.Parse("23873223-1f04-4b9b-8919-a6e8579b0525");
+            Assert.AreEqual(expected, QueryString.GetGuidValue(new Dictionary<string, string> { { "foo", "23873223-1f04-4b9b-8919-a6e8579b0525" } }, "foo"));
+            Assert.AreEqual(expected, QueryString.GetGuidValue(new Dictionary<string, string> { { "foo", "23873223-1F04-4B9B-8919-A6E8579B0525" } }, "foo"));
+            Assert.AreEqual(expected, QueryString.GetGuidValue(new Dictionary<string, string> { { "foo", "238732231F044B9B8919A6E8579B0525" } }, "foo"));
+            Assert.AreEqual(expected, QueryString.GetGuidValue(new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("foo", "23873223-1f04-4b9b-8919-a6e8579b0525"),
+                new KeyValuePair<string, string>("foo", "dc6fb65e-b6e8-423e-a608-a8dd74ed61cd")
+            }, "foo"), "Failed to retrieve first matching key value.");
+        }
+    }
+}
